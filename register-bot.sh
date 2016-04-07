fa45a3efe8e55bcea9f1ee9de899cd68b406b4b6
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-04-07 11:04:50 +0200

    Fix webhook setup script text

--- register-bot.sh
+++ register-bot.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Telegram Bot Sample
+# ===================
+# UWiClab, University of Urbino
+# ===================
+# Basic script to setup the Telegram API webhook.
+# Use with a custom bot token, a webhook HTTPS URL and the HTTPS certificate
+# installed onto the webhook's web server.
+#
+# Note: you *need* a domain and a valid certificate for this to work.
+
 function PrintHelp {
     echo -e "usage: $0 -s <script HTTPS URL> -c <certificate path> -t <bot token>"
-    echo "EXAMPLE: $0 -t 140354381:AAF6me-NAHyXLlZ2eO3d3lEdYKnXuZPd98 -c /home/delpriori/bot/public.pem -s https://chatbot.clouduino.eu/delpriori/bot/"
+    echo "EXAMPLE: $0 -t 123456789:AAF6me-NAHyXLlZ2eO3d3lEdYKnXuZPd98 -c /home/delpriori/bot/public.pem -s https://botify.it/webhook"
     echo ""
 }
 
@@ -42,7 +52,6 @@ if [ -z "$SCRIPT" ]; then
         exit 1;
 fi
 
-echo "executing: curl -F \"url=$SCRIPT\" -F \"certificate=@$CERTIFICATE\" https://api.telegram.org/bot$TOKEN/setWebhook"
+echo "Executing: curl -F \"url=$SCRIPT\" -F \"certificate=@$CERTIFICATE\" https://api.telegram.org/bot$TOKEN/setWebhook"
 
 curl -F "url=$SCRIPT" -F "certificate=@$CERTIFICATE" https://api.telegram.org/bot$TOKEN/setWebhook
-

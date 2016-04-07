b87499ce1e75594131f8862bade45acad6ed0e73
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-04-07 10:49:59 +0200

    Clean configuration

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
  */
 
 define('PROGRAM_O_API_URI','http://chatbot.clouduino.eu/klopfenstein/programo/chatbot/conversation_start.php');
-define('TELEGRAM_BOT_TOKEN', '148168117:AAGV1XjYNEloMdoaf6V2_BYX8YeELgKL_9U');
+define('TELEGRAM_BOT_TOKEN', '');
 define('TELEGRAM_API_URI_BASE', 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN . '/');
 define('TELEGRAM_API_URI_MESSAGE', TELEGRAM_API_URI_BASE . 'sendMessage');
 

d75add3864c1ee49d269e911ef63b0e0e978bd96
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-04-07 18:38:04 +0200

    Reorganize hook and message processing

--- hook.php
+++ hook.php
@@ -8,41 +8,7 @@
  * Start editing here. =)
  */
 
-include ("lib.php");
-
-function processMessage($message)
-{
-  // Extract important information from the message object
-  $message_id = $message['message_id'];
-  $chat_id = $message['chat']['id'];
-
-  if (isset($message['text']))
-  	{
-   	 	// incoming text message
-  	  $text = $message['text'];
-  	if (strpos($text, "/start") === 0)
-		{
-			apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'hei'));
-   	 	}
-		else if (strpos($text, "audio") === 0 || strpos($text, "manda un audio") === 0)
-		{
-			apiRequest("sendDocument", array('chat_id' => $chat_id, "document"   => "BQADBAADDAADr6qEB6PQYEUWsy5KAg"));
-	    }
-		else if (strpos($text, "foto") === 0 || strpos($text, "manda una foto") === 0)
-		{
-			apiRequest("sendPhoto", array('chat_id' => $chat_id, "photo"   => "AgADBAADtKcxG6-qhAc5-dBu7k__u2OLjzAABER-rxPmZC4lpC4CAAEC"));
-	    }
-   	 else
-		{
-	  	  $bot = alicerequest("telegram".$chat_id,$text);
-      	apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "$bot"));
-    	}
-    }
-   else
-   {
-    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
-   }
-}
+include ("lib_msg_processing.php");
 
 // Get contents from HTTP request
 $content = file_get_contents("php://input");
---
+++ lib_msg_processing.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Telegram Bot Sample
+ * ===================
+ * UWiClab, University of Urbino
+ * ===================
+ * Basic message processing functionality,
+ * used by both pull and push scripts.
+ *
+ * Put your custom bot intelligence here!
+ */
+
+/**
+ * Processes a message received through the Telegram API.
+ * @param object $message A message object as parsed from the Telegram API.
+ * @return Nothing.
+ */
+function process_message($message) {
+    // Extract important information from the message object
+    //
+    // Message object structure: {
+    //     "message_id": 123,
+    //     "from": {
+    //       "id": 123456789,
+    //       "first_name": "First",
+    //       "last_name": "Last",
+    //       "username": "FirstLast"
+    //     },
+    //     "chat": {
+    //       "id": 123456789,
+    //       "first_name": "First",
+    //       "last_name": "Last",
+    //       "username": "FirstLast",
+    //       "type": "private"
+    //     },
+    //     "date": 1460036220,
+    //     "text": "Text"
+    //   }
+    $message_id = $message['message_id'];
+    $chat_id = $message['chat']['id'];
+
+    if (isset($message['text'])) {
+        // We got an incoming text message
+        $text = $message['text'];
+
+        if (strpos($text, "/start") === 0) {
+            // Start command
+            // ...do something
+
+            echo 'Received /start command!' . PHP_EOL;
+   	 	}
+        else {
+            // Something else
+            // ...do something else
+
+            echo "Received message: $text!" . PHP_EOL;
+    	}
+    }
+    else {
+        if(telegram_send_message($chat_id, 'Sorry, I understand only text messages!', null) === false) {
+            error_log('Failed to answer in chat');
+        }
+    }
+}
+?>

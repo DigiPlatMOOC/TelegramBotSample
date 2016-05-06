d02530cdca8bea0d8ce0f522a728f21d67a7ec8f
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-05-06 08:08:16 +0000

    Change Program-O default end-point

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
  * Configuration file.
  */
 
-define('PROGRAM_O_API_URI','http://chatbot.clouduino.eu/klopfenstein/programo/chatbot/conversation_start.php');
+define('PROGRAM_O_API_URI','http://api.program-o.com/v2/chatbot');
 define('TELEGRAM_BOT_TOKEN', '');
 define('TELEGRAM_API_URI_BASE', 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN . '/');
 define('TELEGRAM_API_URI_MESSAGE', TELEGRAM_API_URI_BASE . 'sendMessage');

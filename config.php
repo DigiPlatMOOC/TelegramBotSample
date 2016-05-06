5be105b6864e7c5f01a006714eb394c3721c0c83
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-05-06 08:24:09 +0000

    Add new configuration options

--- config.php
+++ config.php
@@ -9,9 +9,15 @@
 
 define('PROGRAM_O_API_URI','http://api.program-o.com/v2/chatbot');
 define('TELEGRAM_BOT_TOKEN', '');
+
 define('TELEGRAM_API_URI_BASE', 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN . '/');
 define('TELEGRAM_API_URI_MESSAGE', TELEGRAM_API_URI_BASE . 'sendMessage');
 define('TELEGRAM_API_URI_LOCATION', TELEGRAM_API_URI_BASE . 'sendLocation');
 define('TELEGRAM_API_URI_UPDATES', TELEGRAM_API_URI_BASE . 'getUpdates');
+
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_NAME', '');
+define('DATABASE_USERNAME', '');
+define('DATABASE_PASSWORD', '');
 
 ?>

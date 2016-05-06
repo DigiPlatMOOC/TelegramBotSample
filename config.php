80e7691e1b051b0b877f19f6e08e58d46bcbdb59
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-05-06 13:08:05 +0000

    Config regrouping

--- config.php
+++ config.php
@@ -8,8 +8,8 @@
  */
 
 define('PROGRAM_O_API_URI','http://api.program-o.com/v2/chatbot');
-define('TELEGRAM_BOT_TOKEN', '');
 
+define('TELEGRAM_BOT_TOKEN', '');
 define('TELEGRAM_API_URI_BASE', 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN . '/');
 define('TELEGRAM_API_URI_MESSAGE', TELEGRAM_API_URI_BASE . 'sendMessage');
 define('TELEGRAM_API_URI_LOCATION', TELEGRAM_API_URI_BASE . 'sendLocation');

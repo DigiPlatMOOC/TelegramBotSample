373d801cbb73b39611215009738723e322b62989
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-05-09 15:38:46 +0200

    Comment clean-up and configuration checking

--- lib_database.php
+++ lib_database.php
@@ -6,7 +6,7 @@
  * ===================
  * Database support library. Don't change a thing here.
  */
- 
+
  function db_default_error_logging($connection, $message = "Database error") {
     $errno = mysqli_errno($connection);
     $error = mysqli_error($connection);
@@ -19,10 +19,10 @@
 function db_close_connection() {
     if(isset($GLOBALS['db_connection'])) {
         $connection = $GLOBALS['db_connection'];
-        
+
         mysqli_close($connection);
     }
-    
+
     unset($GLOBALS['db_connection']);
 }
 
@@ -34,36 +34,42 @@ function db_close_connection() {
 function db_open_connection($quick = false) {
     if(isset($GLOBALS['db_connection'])) {
         $connection = $GLOBALS['db_connection'];
-        
+
         if(!$quick) {
-            //Ping the connection just to be safe
-            //This can be removed for performance since we usually have no
-            //long-running scripts.
+            // Ping the connection just to be safe
+            // This can be removed for performance since we usually have no
+            // long-running scripts.
             if(!mysqli_ping($connection)) {
                 error_log('Database connection already open but does not respond to ping');
                 die();
             }
         }
-        
+
         return $connection;
     }
     else {
-        //Open up a new connection
+        // Check configuration
+        if(!DATABASE_USERNAME || !DATABASE_NAME) {
+            error_log('Please configure the database connection in file config.php');
+            die();
+        }
+
+        // Open up a new connection
         $connection = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME);
-        
+
         if(!$connection) {
             $errno = mysqli_connect_errno();
             $error = mysqli_connect_error();
-            error_log("Failed to establish database connection. Err #$errno: $error");
+            error_log("Failed to establish database connection. Error #$errno: $error");
             die();
         }
-        
-        //Store connection for later
+
+        // Store connection for later
         $GLOBALS['db_connection'] = $connection;
-        
-        //Register clean up function for termination
+
+        // Register clean up function for termination
         register_shutdown_function('db_close_connection');
-        
+
         return $connection;
     }
 }
@@ -76,18 +82,18 @@ function db_open_connection($quick = false) {
  */
 function db_perform_action($sql) {
     $connection = db_open_connection();
-    
+
     if(!mysqli_real_query($connection, $sql)) {
         db_default_error_logging($connection);
         return false;
     }
-    
+
     $affected_rows = mysqli_affected_rows($connection);
     if($affected_rows < 0) {
         db_default_error_logging($connection, "Failed to check for affected rows");
         return false;
     }
-    
+
     return $affected_rows;
 }
 
@@ -100,19 +106,19 @@ function db_perform_action($sql) {
  */
 function db_scalar_query($sql) {
     $connection = db_open_connection();
-    
+
     if(!mysqli_real_query($connection, $sql)) {
         db_default_error_logging($connection);
         return false;
     }
-    
+
     $result = mysqli_store_result($connection);
     if($result === false) {
         db_default_error_logging($connection, "Failed to store query results");
         return false;
     }
-    
-    //Sanity checks
+
+    // Sanity checks
     if(mysqli_field_count($connection) !== 1) {
         mysqli_free_result($result);
         error_log("Query $sql generated results with multiple fields (non-scalar)");
@@ -123,11 +129,11 @@ function db_scalar_query($sql) {
         error_log("Query $sql generated more than one row of results (non-scalar)");
         return false;
     }
-    
-    //Extract first row
+
+    // Extract first row
     $row = mysqli_fetch_row($result);
     mysqli_free_result($result);
-    
+
     //Error checking on results (just for the sake of it)
     if($row == null) {
         error_log("Failed to access first row of query results");
@@ -137,8 +143,8 @@ function db_scalar_query($sql) {
         error_log("Results row is empty");
         return false;
     }
-    
-    //Extract and return first field
+
+    // Extract and return first field
     return $row[0];
 }
 
@@ -151,12 +157,12 @@ function db_scalar_query($sql) {
  */
 function db_table_query($sql) {
     $connection = db_open_connection();
-    
+
     if(!mysqli_real_query($connection, $sql)) {
         db_default_error_logging($connection);
         return false;
     }
-    
+
     $result = mysqli_store_result($connection);
     if($result === false) {
         db_default_error_logging($connection, "Failed to store query results");
@@ -167,9 +173,9 @@ function db_table_query($sql) {
     while(($row = mysqli_fetch_row($result)) != null) {
         $matrix[] = $row;
     }
-    
+
     mysqli_free_result($result);
-    
+
     return $matrix;
 }
 
